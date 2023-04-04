fb1ea722e0e24f4a3715a3ac064eb7fb07b9f6df
Samora Chumbi <samorachumbi@gmail.com>
2023-04-04 14:05:08 +0300

    updated the server url in Customizer to reflect the onrender url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -56,7 +56,7 @@ const Customizer = () => {
     try {
       // call our backend to generate an A.I. image
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://twond-project-threejs-ai.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
